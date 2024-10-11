8f4079e4edc108669f8adab75a49c3dc5072c889
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-11 14:16:07 -0700

    Added constructors, getters, and setters for the Objects

--- src/main/java/com/pluralsight/Ledger.java
+++ src/main/java/com/pluralsight/Ledger.java
@@ -5,4 +5,16 @@ import java.util.List;
 
 public class Ledger {
     private List<Transaction> transactionList = new ArrayList<>();
+
+    public Ledger(List<Transaction> transactionList) {
+        this.transactionList = transactionList;
+    }
+
+    public List<Transaction> getTransactionList() {
+        return transactionList;
+    }
+
+    public void setTransactionList(List<Transaction> transactionList) {
+        this.transactionList = transactionList;
+    }
 }
--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -6,7 +6,39 @@ import java.time.LocalTime;
 public class Transaction {
     private LocalDate transactionDate;
     private LocalTime transactionTime;
-    private String item;
-    private float itemPrice;
+    private Product product;
 
+    public Transaction(LocalDate transactionDate, LocalTime transactionTime, Product product) {
+        this.transactionDate = transactionDate;
+        this.transactionTime = transactionTime;
+        this.product = product;
+    }
+
+    public Transaction(Product product){
+        this(LocalDate.now(), LocalTime.now(), product);
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public LocalTime getTransactionTime() {
+        return transactionTime;
+    }
+
+    public void setTransactionTime(LocalTime transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
