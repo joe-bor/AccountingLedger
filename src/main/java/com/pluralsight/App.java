f1f0a7f06eda1f6c0cda95e415b3f98eefc06fd0
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-12 21:51:17 -0700

    Added funnction to initialize ledger's transactions based on the transactions.csv file

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,14 +1,18 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class App {
     private static Scanner scanner = new Scanner(System.in);
-    private  static boolean isAppRunning = true;
-    private static Ledger ledger = new Ledger(new ArrayList<Transaction>());
+    private static boolean isAppRunning = true;
+    private static Ledger ledger = initLedger();
 
 
     public static void main(String[] args) {
@@ -18,7 +22,7 @@ public class App {
         } while (isAppRunning);
     }
 
-    private static void greetings(){
+    private static void greetings() {
         System.out.println("""
                 
                 ====== Accounting Ledger =====
@@ -29,6 +33,7 @@ public class App {
                 placeholder.
                 """);
     }
+
     private static void homeScreen() {
         System.out.println("""
                 
@@ -42,9 +47,9 @@ public class App {
         executeHomeScreenOptions();
     }
 
-    private static void executeHomeScreenOptions(){
+    private static void executeHomeScreenOptions() {
         String option = scanner.nextLine().trim().toUpperCase();
-        switch (option){
+        switch (option) {
             case "D" -> addDeposit();
             case "P" -> makePayment();
             case "L" -> goToLedgerScreen();
@@ -53,7 +58,7 @@ public class App {
         }
     }
 
-    private static void quitApp(){
+    private static void quitApp() {
         System.out.println("Quitting the application...");
         setIsAppRunning(false);
     }
@@ -66,7 +71,7 @@ public class App {
         App.isAppRunning = isAppRunning;
     }
 
-    private static void addDeposit(){
+    private static void addDeposit() {
         // prompt user
         System.out.println("What are you depositing?");
         String description = scanner.nextLine().trim();
@@ -85,7 +90,7 @@ public class App {
         logToCSVFile(transaction);
     }
 
-    private static void makePayment(){
+    private static void makePayment() {
         System.out.println("What did you buy?");
         String productDescription = scanner.nextLine().trim();
 
@@ -108,7 +113,7 @@ public class App {
         String vendor = transaction.getProduct().vendor();
         float price = transaction.getProduct().price();
 
-        try{
+        try {
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("transactions.csv", true));
             bufferedWriter.append(String.format("%s|%s|%s|%s|%.2f\n", dateStr, timeStr, description, vendor, price));
             System.out.println("Success! Transaction recorded to `transactions.csv`");
@@ -118,8 +123,31 @@ public class App {
         }
     }
 
-    private static void goToLedgerScreen(){
+    private static void goToLedgerScreen() {
         System.out.println("Switching to Ledger Screen...");
         ledger.displayScreen();
+    }
+
+    // Initializes the ledger's transactions field based on the file 'transactions.csv'
+    private static Ledger initLedger() {
+        List<Transaction> transactionList = new ArrayList<>();
+
+        // This logic assumes 'transactions.csv' is formatted correctly
+        // date|time|description|vendor|amount
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv"))) {
+            String transactionLine = bufferedReader.readLine(); // skips the 1st line (headers)
+            while ((transactionLine = bufferedReader.readLine()) != null) {
+                String[] transactionDetails = transactionLine.trim().split(Pattern.quote("|"));
+                LocalDate transactionDate = LocalDate.parse(transactionDetails[0]);
+                LocalTime transactionTime = LocalTime.parse(transactionDetails[1]);
+                // construct product to be added to transaction list
+                Product product = new Product(transactionDetails[2], transactionDetails[3], Float.parseFloat(transactionDetails[4]));
+                transactionList.add(new Transaction(transactionDate, transactionTime, product));
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        return new Ledger(transactionList);
     }
 }
